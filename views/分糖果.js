340bdff4d9b13a26b4341f1568300d7de56f87b9
Sally7 <38527251+Sally7@users.noreply.github.com>
2022-07-04 13:59:37 +0800

    fix:提交

--- views/分糖果.js
+++ views/分糖果.js
@@ -1,15 +1,13 @@
-int main(){
-    int N;
-    scanf("%d",&N);
-    int candy;
-    int min=1000001;
-    int sum=0;
-    int count=0;                                                //不要忘记初始化
-    for(int i=0;i<N;i++){
-        scanf("%d",&candy);
+function main(){
+    let N;
+    let  candy;
+    let min=1000001;
+    let sum=0;
+    let count=0;                                                //不要忘记初始化
+    for(let i=0;i<N;i++){
         sum+=candy;
-        count^=candy;
-        if(candy<min){
+        count ^=candy;
+        if(candy < min){
             min=candy;                                               //找最小值
         }
     }
